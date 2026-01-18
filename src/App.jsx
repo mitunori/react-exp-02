b83aaad165ecf0bc4754eb5e7e4056020b05542e
mitukun32 <mitukun32@gmail.com>
2026-01-18 13:42:20 +0900

    01回目

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import News from "./components/News";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // inputが２個あるので、入力したものを保持するためにuseStateが２ついります🤗
+  const [name, setName] = useState("名前入力");
+  const [email, setEmail] = useState("メールアドレス入力");
+
+  // イベント処理=クリックしたらXXXする,マウスを動かしたらXXXする、マウスが離れたらxxxx,.....
+  const handleNameChange = (e) => {
+    // 処理を書きます🤗
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    // 処理を書きます🤗
+    setEmail(e.target.value);
+  };
+
+  useEffect(() => {
+    // この中に書きます🤗
+    console.log("起動しました！！！！！！");
+    //スプレッドシートAPIを記述を書いて、データを読み込む→読み込みができたら画面に表示する
+    // この下は消さない
+  }, []);
+
+  // useEffectの前に追加でコンソールログを以下に書きます↓
+  console.log("順番の確認🤗");
 
   return (
     <>
+      {/*  */}
+      <News />
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>名前が入ります</p>
+        <input
+          type="text"
+          placeholder="名前を入力してください"
+          value={name}
+          onChange={handleNameChange}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <p>メールアドレスが入ります</p>
+        <input
+          type="text"
+          placeholder="メールアドレスを入力してください"
+          value={email}
+          onChange={handleEmailChange}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/*  */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
